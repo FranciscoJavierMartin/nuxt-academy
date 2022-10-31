492155ff77c120d16638deb1a4f457284c6caf23
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-31 13:45:09 +0100

    Get params from route

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,6 +1,24 @@
 <template>
   <h2>Lesson</h2>
   <p>This is a lesson</p>
-  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  <p>Chapter slug: {{ chapter.title }}</p>
+  <p>Lesson slug: {{ lesson.title }}</p>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+</script>
