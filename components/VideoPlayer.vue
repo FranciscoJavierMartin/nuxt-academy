492f1426ede9f5cff65dbf73e3c79338858bb5c6
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-31 20:00:17 +0100

    Add video player

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media;  gyroscope; picture-in-picture"
+    allowfullscreen
+  />
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  videoId: {
+    type: String,
+    required: true,
+  },
+});
+</script>
