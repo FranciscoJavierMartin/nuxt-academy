1a6cf90fc8d487a49a1ca3e4e8362f0fed7075f4
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-12-05 19:29:34 +0100

    Add type for course

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,11 +1,33 @@
 import courseData from '@/courseData';
 
-export const useCourse = () => {
+type Lesson = {
+  title: string;
+  slug: string;
+  number: number;
+  downloadUrl: string;
+  videoId: number;
+  text: string;
+  sourceUrl?: string;
+};
+
+type Chapter = {
+  title: string;
+  slug: string;
+  number: number;
+  lessons: Lesson[];
+};
+
+type Course = {
+  title: string;
+  chapters: Chapter[];
+};
+
+export const useCourse = (): Course => {
   return {
     ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
+    chapters: courseData.chapters.map<Chapter>((chapter) => ({
       ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
+      lessons: chapter.lessons.map<Lesson>((lesson) => ({
         ...lesson,
         path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
       })),
