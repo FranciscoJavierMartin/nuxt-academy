7a8f780ea4a325dd3baaf7c8295aad134a9a1629
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-12-06 08:45:19 +0100

    Remove console.log

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,7 +2,6 @@ import { RouteLocationNormalized } from 'vue-router';
 
 export default defineNuxtRouteMiddleware(
   (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
-    console.log('1');
     if (to.params.chapterSlug === '1-chapter-1') {
       return;
     }
