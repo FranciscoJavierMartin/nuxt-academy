6c44f36e83859ca6b545a886baccc14f0da9d8a0
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-12-14 20:44:50 +0100

    use lesson with cache

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,22 +1,12 @@
-import { LessonWithPath } from '@/types/course';
 import { Ref } from 'vue';
+import { LessonWithPath } from '@/types/course';
 
 const useLesson = async (
   chapterSlug: string,
   lessonSlug: string
-): Promise<Ref<LessonWithPath>> => {
-  const { data, error } = await useFetch(
+): Promise<Ref<LessonWithPath>> =>
+  useFetchWithCache<LessonWithPath>(
     `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
   );
-
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-    });
-  }
-
-  return data as Ref<LessonWithPath>;
-};
 
 export default useLesson;
