0cb1a6a0fe54c12895906692d99225c645763fa8
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-07 21:17:08 +0100

    Fix path

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,3 +1,31 @@
-import { CourseOutline } from '@/server/api/course/meta.get';
+import {
+  ChapterOutline,
+  CourseOutline,
+  LessonOutline,
+} from '@/server/api/course/meta.get';
 
-export default async () => useFetchWithCache<CourseOutline>('/api/course/meta');
+export type LessonOutlineWithPath = LessonOutline & {
+  path: string;
+};
+
+export type ChapterOutlineWithPath = ChapterOutline & {
+  lessons: LessonOutlineWithPath[];
+};
+
+export default async () => {
+  const course = await useFetchWithCache<CourseOutline>('/api/course/meta');
+
+  const chaptersOutline: ChapterOutline[] = course.value.chapters.map(
+    (chapterOutline: ChapterOutline) => ({
+      ...chapterOutline,
+      lessons: chapterOutline.lessons.map((lesson: LessonOutline) => ({
+        ...lesson,
+        path: '',
+      })),
+    })
+  );
+
+  course.value.chapters = chaptersOutline;
+
+  return course;
+};
