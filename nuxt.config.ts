3259c75b86667e3b525bebf5f8392d1f4e552ace
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-21 21:37:27 +0100

    Add static assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
+import vsharp from 'vite-plugin-vsharp';
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ['/landing'],
+    },
+  },
+  vite: {
+    plugins: [vsharp()],
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
