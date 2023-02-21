df1bde9e3af2d76e2a33382f953c484c3165962c
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-21 21:02:27 +0100

    Add landing page

---
+++ components/Section.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section bg-white p-24 flex flex-col items-center">
+    <div class="w-full max-w-4xl">
+      <h2 v-if="title" class="text-4xl font-bold pb-8 text-center">
+        {{ title }}
+      </h2>
+      <div v-bind="$attrs">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{ title?: string }>();
+</script>
+
+<style>
+.section:nth-of-type(even) {
+  @apply bg-blue-500 text-white;
+}
+</style>
